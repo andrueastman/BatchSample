66e4880964ef745cee78d19f1155c970e525492d
Andrew Omondi <v-anomon@microsoft.com>
2019-07-18 16:06:00 +0300

    First Commit

---
+++ EnhancedBatch/HttpQuery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+
+namespace EnhancedBatch
+{
+    internal class HttpQuery
+    {
+        public GraphServiceClient GraphClient { get; }
+        private readonly List<Task> _requestCollection;
+        public int BatchSize { get; private set; } = 20;
+
+        public HttpQuery(GraphServiceClient graphClient)
+        {
+            GraphClient = graphClient;
+            _requestCollection = new List<Task>();//empty at start
+        }
+
+        public void AddRequest<T>(IBaseRequest request, Func<T, T> handlerFunc)
+        {
+            HttpRequestMessage httpRequestMessage = request.GetHttpRequestMessage();
+            Task task = GraphClient.HttpProvider.SendAsync(httpRequestMessage).ContinueWith(
+                async t =>
+                {
+                    if (t.IsCompleted)
+                    {
+                        HttpResponseMessage response = t.Result;
+                        if (response.Content != null)
+                        {
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            T variaDeserializeObject = GraphClient.HttpProvider.Serializer.DeserializeObject<T>(responseString);
+                            handlerFunc(variaDeserializeObject);
+                        }
+                    }
+                }
+            );
+
+            _requestCollection.Add(task);
+        }
+
+        public void ConfigureBatchSize(int size)
+        {
+            BatchSize = size;
+        }
+
+        public void ExecuteAsync()
+        {
+            Task.WaitAll(_requestCollection.ToArray());
+        }
+    }
+}
---
+++ EnhancedBatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace EnhancedBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Do the auth stuff first */
+            string clientId = "d662ac70-7482-45af-9dc3-c3cde8eeede4";
+            string[] scopes = new string[] { "User.Read", "Calendars.Read"};
+
+            IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+                .Create(clientId).WithRedirectUri("http://localhost:1234")
+                .Build();
+
+            InteractiveAuthenticationProvider authenticationProvider = new InteractiveAuthenticationProvider(publicClientApplication, scopes);
+
+            /* Get the client */
+            GraphServiceClient graphClient = new GraphServiceClient(authenticationProvider);
+
+            User user = null;
+            Photo photo = null;
+            MailFolderMessagesCollectionPage mail = null;
+
+            var user2 = graphClient.Me.Request().GetAsync().Result;
+
+            var query = new HttpQuery(graphClient);
+            query.AddRequest<User>(graphClient.Me.Request(), u => user = u);
+            query.AddRequest<Photo>(graphClient.Me.Calendar.Events.Request(), p => photo = p);
+            query.AddRequest<MailFolderMessagesCollectionPage>(graphClient.Me.MailFolders.Inbox.Messages.Request(), m => mail = m);
+
+            query.ExecuteAsync();
+
+            Console.WriteLine("Display Name: " + user.DisplayName);
+            Console.WriteLine("Photo: " + photo.TakenDateTime);
+
+            foreach (var email in mail)
+            {
+                Console.WriteLine("Email: " + email.Subject);
+            }
+        }
+    }
+}
